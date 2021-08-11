bcf6261891824c6c04fbf61351fef180610dfcb3
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-11 11:18:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -9,20 +9,20 @@ load_dotenv()
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "Veez Music")
-BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/d91623e9cefcef8fb3aa8.png")
+BOT_NAME = getenv("BOT_NAME", "ᴄʏʙᴇʀ ᴍᴜsɪᴄ ʙᴏᴛ")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/ec11307dd8a3496d8dcbf.jpg")
 admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "veezmusicbot")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "veezassistant")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "VeezSupportGroup")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "levinachannel")
-OWNER_NAME = getenv("OWNER_NAME", "dlwrml") # isi dengan username kamu tanpa simbol @
-DEV_NAME = getenv("DEV_NAME", "dlwrml")
+BOT_USERNAME = getenv("BOT_USERNAME", "CyberMusikBot")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "SaitamaHelper")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "CyberSupportGroup")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "CyberMusicProject")
+OWNER_NAME = getenv("OWNER_NAME", "Badboyanim") # isi dengan username kamu tanpa simbol @
+DEV_NAME = getenv("DEV_NAME", "Badboyanim")
 PMPERMIT = getenv("PMPERMIT", None)
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "250"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 

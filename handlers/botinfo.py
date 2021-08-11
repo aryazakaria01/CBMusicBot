7ef43bfdaed784dd89ec584e7351f81c2e3f5838
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-11 12:08:24 +0700

    Update botinfo.py

--- handlers/botinfo.py
+++ handlers/botinfo.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 By VeezProject
+# Copyright (C) 2021 By CyberProject
 
 import os
 from config import OWNER_NAME, UPDATES_CHANNEL, GROUP_SUPPORT, BOT_NAME, DEV_NAME
@@ -7,9 +7,9 @@ class Messages():
     ".",
 f"""
 **󠁧󠁢󠁥󠁮󠁧󠁿✨ {BOT_NAME} information panel ✨ \n
-💭 as you know, veez music is a telegram music bot project for streaming music on telegram voice chat group feature.\n
-💠 veez music project was founded on March 23, 2021, we created a music bot project for streaming on telegram voice chat, which is now known as veez music bot.\n
-🧪 veez music version:
+💭 as you know, cyber music is a telegram music bot project for streaming music on telegram voice chat group feature.\n
+💠 cyber music project was founded on April 12, 2020, we created a music bot project for streaming on telegram voice chat, which is now known as cyber music bot.\n
+🧪 cyber music version:
 ❍ ver.6.0
 ❍ beta-version \n
 👩🏻‍💻 developer:
@@ -19,7 +19,5 @@ f"""
 ❍ @{GROUP_SUPPORT}
 📣 updates channel:
 ❍ @{UPDATES_CHANNEL}\n
-💝 donation page:
-⊛ You can donate sincerely to us through our donation page which you can visit via the following link: https://trakteer.id/levina-shavila-dp0ju**
 """
   ]

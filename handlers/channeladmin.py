2d461d0b3605e0541e10a08306d94455b44b4717
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-14 12:58:43 +0700

    Create channeladmin.py

---
+++ handlers/channeladmin.py
@@ -0,0 +1,130 @@
+from asyncio.queues import QueueEmpty
+from config import que
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from cache.admins import set
+from helpers.channelmusic import get_chat_id
+from helpers.decorators import authorized_users_only, errors
+from helpers.filters import command, other_filters
+from callsmusic import callsmusic
+
+
+
+@Client.on_message(filters.command(["channelpause","cpause"]) & filters.group & ~filters.edited)
+@errors
+@authorized_users_only
+async def pause(_, message: Message):
+    try:
+      conchat = await _.get_chat(message.chat.id)
+      conid = conchat.linked_chat.id
+      chid = conid
+    except:
+      await message.reply("ɪs ʏᴏᴜʀ ᴄʜᴀᴛ ᴀʟʀᴇᴀᴅʏ ᴄᴏɴɴᴇᴄᴛᴇᴅ ?")
+      return    
+    chat_id = chid
+    if (chat_id not in callsmusic.pytgcalls.active_calls) or (
+        callsmusic.pytgcalls.active_calls[chat_id] == "paused"
+    ):
+        await message.reply_text("✘ ɴᴏᴛʜɪɴɢ ɪs ᴘʟᴀʏɪɴɢ !")
+    else:
+        callsmusic.pytgcalls.pause_stream(chat_id)
+        await message.reply_text("➤ ᴍᴜsɪᴄ ᴘᴀᴜsᴇᴅ!")
+
+
+@Client.on_message(filters.command(["channelresume","cresume"]) & filters.group & ~filters.edited)
+@errors
+@authorized_users_only
+async def resume(_, message: Message):
+    try:
+      conchat = await _.get_chat(message.chat.id)
+      conid = conchat.linked_chat.id
+      chid = conid
+    except:
+      await message.reply("ɪs ʏᴏᴜʀ ᴄʜᴀᴛ ᴀʟʀᴇᴀᴅʏ ᴄᴏɴɴᴇᴄᴛᴇᴅ ?")
+      return    
+    chat_id = chid
+    if (chat_id not in callsmusic.pytgcalls.active_calls) or (
+        callsmusic.pytgcalls.active_calls[chat_id] == "playing"
+    ):
+        await message.reply_text("✘ ɴᴏᴛʜɪɴɢ ɪs ᴘᴀᴜsᴇᴅ!")
+    else:
+        callsmusic.pytgcalls.resume_stream(chat_id)
+        await message.reply_text("✈ ᴍᴜsɪᴄ ʀᴇsᴜᴍᴇᴅ!")
+
+
+@Client.on_message(filters.command(["channelend","cend"]) & filters.group & ~filters.edited)
+@errors
+@authorized_users_only
+async def stop(_, message: Message):
+    try:
+      conchat = await _.get_chat(message.chat.id)
+      conid = conchat.linked_chat.id
+      chid = conid
+    except:
+      await message.reply("ɪs ʏᴏᴜʀ ᴄʜᴀᴛ ᴀʟʀᴇᴀᴅʏ ᴄᴏɴɴᴇᴄᴛᴇᴅ ?")
+      return    
+    chat_id = chid
+    if chat_id not in callsmusic.pytgcalls.active_calls:
+        await message.reply_text("✘ ɴᴏᴛʜɪɴɢ ɪɴ sᴛʀᴇᴀᴍɪɴɢ!")
+    else:
+        try:
+            callsmusic.queues.clear(chat_id)
+        except QueueEmpty:
+            pass
+
+        callsmusic.pytgcalls.leave_group_call(chat_id)
+        await message.reply_text("♨ sᴛʀᴇᴀᴍɪɴɢ ᴇɴᴅᴇᴅ!")
+
+
+@Client.on_message(filters.command(["channelskip","cskip"]) & filters.group & ~filters.edited)
+@errors
+@authorized_users_only
+async def skip(_, message: Message):
+    global que
+    try:
+      conchat = await _.get_chat(message.chat.id)
+      conid = conchat.linked_chat.id
+      chid = conid
+    except:
+      await message.reply("ɪs ᴛʜᴇ ᴄʜᴀᴛ ᴀʟʀᴇᴀᴅʏ ᴄᴏɴɴᴇᴄᴛᴇᴅ ?")
+      return    
+    chat_id = chid
+    if chat_id not in callsmusic.pytgcalls.active_calls:
+        await message.reply_text("♨ ɴᴏᴛʜɪɴɢ ᴛᴏ sᴋɪᴘ!")
+    else:
+        callsmusic.queues.task_done(chat_id)
+
+        if callsmusic.queues.is_empty(chat_id):
+            callsmusic.pytgcalls.leave_group_call(chat_id)
+        else:
+            callsmusic.pytgcalls.change_stream(
+                chat_id, callsmusic.queues.get(chat_id)["file"]
+            )
+
+    qeue = que.get(chat_id)
+    if qeue:
+        skip = qeue.pop(0)
+    if not qeue:
+        return
+    await message.reply_text(f"- sᴋɪᴘᴘᴇᴅ **{skip[0]}**\n- ɴᴏᴡ ᴘʟᴀʏɪɴɢ **{qeue[0][0]}**")
+
+
+@Client.on_message(filters.command("admincache"))
+@errors
+async def admincache(client, message: Message):
+    try:
+      conchat = await client.get_chat(message.chat.id)
+      conid = conchat.linked_chat.id
+      chid = conid
+    except:
+      await message.reply("ɪs ᴛʜᴇ ᴄʜᴀᴛ ᴀʟʀᴇᴀᴅʏ ᴄᴏɴɴᴇᴄᴛᴇᴅ ?")
+      return
+    set(
+        chid,
+        [
+            member.user
+            for member in await conchat.linked_chat.get_members(filter="administrators")
+        ],
+    )
+    await message.reply_text("✔ ᴀᴅᴍɪɴ ᴄᴀᴄʜᴇ ʀᴇғʀᴇsʜᴇᴅ!")

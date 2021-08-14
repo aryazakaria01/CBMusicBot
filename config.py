cc9fc5d8d3d66251b1ea1806d3c988a5fbb379da
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-14 15:55:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,6 +11,9 @@ SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME", "ᴄʏʙᴇʀ ᴍᴜsɪᴄ ʙᴏᴛ")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/ec11307dd8a3496d8dcbf.jpg")
+THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/bad69dc7929731b11e056.jpg")
+AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/49342251174d89e1a671e.png")
+QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/5c4e60fc17c45d56f39e8.png")
 admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
@@ -22,7 +25,7 @@ OWNER_NAME = getenv("OWNER_NAME", "Badboyanim") # isi dengan username kamu tanpa
 DEV_NAME = getenv("DEV_NAME", "Badboyanim")
 PMPERMIT = getenv("PMPERMIT", None)
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "250"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 

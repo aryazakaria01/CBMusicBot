7fa0d763ff5fc2c15c86ce843f7c5629d8fd364d
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-14 14:46:12 +0700

    Update __init__.py

--- converter/__init__.py
+++ converter/__init__.py
@@ -1,8 +1,8 @@
 from os import listdir, mkdir
 
 if "raw_files" not in listdir():
-  mkdir("raw_files")
+    mkdir("raw_files")
 
-from .converter import convert
+from converter.converter import convert
 
 __all__ = ["convert"]

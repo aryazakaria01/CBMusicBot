07295b6f053453cc349f68dbe8fba5f4fd19f067
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-14 14:34:49 +0700

    Create entertainment.py

---
+++ handlers/entertainment.py
@@ -0,0 +1,72 @@
+# Module by https://github.com/tofikdn
+# Copyright (C) 2021 TdMusic
+# Ported by @aryazakaria01 for CBMusicBot
+
+import requests
+from pyrogram import Client
+from config import BOT_USERNAME
+from helpers.filters import command
+
+@Client.on_message(command(["asupan", f"asupan@{BOT_USERNAME}"]))
+async def asupan(client, message):
+    try:
+        resp = requests.get("https://tede-api.herokuapp.com/api/asupan/ptl").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ ʟᴏʟ...`")
+
+
+@Client.on_message(command(["wibu", f"wibu@{BOT_USERNAME}"]))
+async def wibu(client, message):
+    try:
+        resp = requests.get("https://tede-api.herokuapp.com/api/asupan/wibu").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ ʟᴏʟ...`")
+
+
+@Client.on_message(command(["chika", f"chika@{BOT_USERNAME}"]))
+async def chika(client, message):
+    try:
+        resp = requests.get("https://tede-api.herokuapp.com/api/chika").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ ʟᴏʟ...`")
+
+
+@Client.on_message(command(["truth", f"truth@{BOT_USERNAME}"]))
+async def truth(client, message):
+    try:
+        resp = requests.get("https://tede-api.herokuapp.com/api/truth").json()
+        results = f"{resp['message']}"
+        return await message.reply_text(results)
+    except Exception:
+        await message.reply_text("sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ...")
+
+
+@Client.on_message(command(["dare", f"dare@{BOT_USERNAME}"]))
+async def dare(client, message):
+    try:
+        resp = requests.get("https://tede-api.herokuapp.com/api/dare").json()
+        results = f"{resp['message']}"
+        return await message.reply_text(results)
+    except Exception:
+        await message.reply_text("sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ...")
+
+
+@Client.on_message(command(["lyric", f"lyric@{bu}"]))
+async def lirik(_, message):
+    try:
+        if len(message.command) < 2:
+            await message.reply_text("**ɢɪᴠᴇ ᴀ ʟʏʀɪᴄ ɴᴀᴍᴇ ᴛᴏᴏ !**")
+            return
+        query = message.text.split(None, 1)[1]
+        rep = await message.reply_text("✈ **sᴇᴀʀᴄʜɪɴɢ ʟʏʀɪᴄs...**")
+        resp = requests.get(f"https://tede-api.herokuapp.com/api/lirik?l={query}").json()
+        result = f"{resp['data']}"
+        await rep.edit(result)
+    except Exception:
+        await rep.edit("**ʟʏʀɪᴄs ɴᴏᴛ ғᴏᴜɴᴅ.** ᴘʟᴇᴀsᴇ ɢɪᴠᴇ ᴀ ᴠᴀʟɪᴅ sᴏɴɢ ɴᴀᴍᴇ !")

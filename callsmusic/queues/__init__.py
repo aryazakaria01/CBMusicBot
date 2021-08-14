8e63d004989f132c7615eeb5b21b84fb030e910c
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-14 14:47:13 +0700

    Update __init__.py

--- callsmusic/queues/__init__.py
+++ callsmusic/queues/__init__.py
@@ -1,3 +1,3 @@
-from .queues import clear, get, is_empty, put, task_done
+from callsmusic.queues.queues import clear, get, is_empty, put, task_done
 
 __all__ = ["clear", "get", "is_empty", "put", "task_done"]

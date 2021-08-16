cd9b63a616c0248365ccbfb0752472004148d445
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-16 13:28:23 +0700

    Bug Fixed

--- handlers/admins.py
+++ handlers/admins.py
@@ -1,31 +1,31 @@
+# Copyright (C) 2021 CyberMusicProject
+
+
 from asyncio import QueueEmpty
-from cache.admins import set
-from cache.admins import admins
+from config import que
 from pyrogram import Client, filters
 from pyrogram.types import Message
+
+from cache.admins import admins
+from helpers.channelmusic import get_chat_id
+from helpers.decorators import authorized_users_only, errors
+from helpers.filters import command, other_filters
 from callsmusic import callsmusic
 from callsmusic.queues import queues
-from helpers.channelmusic import get_chat_id
-from helpers.filters import command, other_filters
-from helpers.decorators import errors, authorized_users_only
-from config import BOT_USERNAME, que, admins
-
-@Client.on_message(command(["reload", f"reload@{BOT_USERNAME}"]) & other_filters)
-@errors
-@authorized_users_only
-async def update_admin(client, message: Message):
-    chat_id = get_chat_id(message.chat)
-    set(
-        chat_id,
-        [
-            member.user
-            for member in await message.chat.get_members(filter="administrators")
-        ],
-    )
-    await message.reply_text("✔ Bot **ʀᴇʟᴏᴀᴅᴇᴅ ᴄᴏʀʀᴇᴄᴛʟʏ !**\n\n• **ᴀᴅᴍɪɴ ʟɪsᴛ** ʜᴀs ʙᴇᴇɴ **ᴜᴘᴅᴀᴛᴇᴅ.**")
 
 
-@Client.on_message(command(["pause", f"pause@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(filters.command("reload"))
+async def update_admin(client, message):
+    global admins
+    new_admins = []
+    new_ads = await client.get_chat_members(message.chat.id, filter="administrators")
+    for u in new_ads:
+        new_admins.append(u.user.id)
+    admins[message.chat.id] = new_admins
+    await message.reply_text("✅ Bot **reloaded correctly !**\n✅ **Admin list** has been **updated !**")
+
+
+@Client.on_message(command("pause") & other_filters)
 @errors
 @authorized_users_only
 async def pause(_, message: Message):
@@ -33,13 +33,13 @@ async def pause(_, message: Message):
     if (chat_id not in callsmusic.pytgcalls.active_calls) or (
         callsmusic.pytgcalls.active_calls[chat_id] == "paused"
     ):
-        await message.reply_text("☣ ɴᴏᴛʜɪɴɢ ɪs ᴘʟᴀʏɪɴɢ!")
+        await message.reply_text("❗ nothing in streaming!")
     else:
         callsmusic.pytgcalls.pause_stream(chat_id)
-        await message.reply_text("➤ ᴍᴜsɪᴄ ᴘᴀᴜsᴇᴅ!")
+        await message.reply_text("▶️ music paused!")
 
 
-@Client.on_message(command(["resume", f"resume@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command("resume") & other_filters)
 @errors
 @authorized_users_only
 async def resume(_, message: Message):
@@ -47,63 +47,82 @@ async def resume(_, message: Message):
     if (chat_id not in callsmusic.pytgcalls.active_calls) or (
         callsmusic.pytgcalls.active_calls[chat_id] == "playing"
     ):
-        await message.reply_text("☣ ɴᴏᴛʜɪɴɢ ɪs ᴘᴀᴜsᴇᴅ!")
+        await message.reply_text("❗ nothing is paused!")
     else:
         callsmusic.pytgcalls.resume_stream(chat_id)
-        await message.reply_text("✈ ᴍᴜsɪᴄ ʀᴇsᴜᴍᴇᴅ!")
+        await message.reply_text("⏸ music resumed!")
 
 
-@Client.on_message(command(["end", f"end@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command("end") & other_filters)
 @errors
 @authorized_users_only
 async def stop(_, message: Message):
     chat_id = get_chat_id(message.chat)
     if chat_id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("☣ ɴᴏᴛʜɪɴɢ ɪɴ sᴛʀᴇᴀᴍɪɴɢ!")
+        await message.reply_text("❗ nothing in streaming!")
     else:
         try:
-            callsmusic.queues.clear(chat_id)
+            queues.clear(chat_id)
         except QueueEmpty:
             pass
 
         callsmusic.pytgcalls.leave_group_call(chat_id)
-        await message.reply_text("✘ ᴍᴜsɪᴄ sᴛᴏᴘᴘᴇᴅ!")
+        await message.reply_text("⏹ streaming ended!")
 
 
-@Client.on_message(command(["skip", f"skip@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command("skip") & other_filters)
 @errors
 @authorized_users_only
 async def skip(_, message: Message):
     global que
     chat_id = get_chat_id(message.chat)
     if chat_id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("☣ ɴᴏᴛʜɪɴɢ ᴛᴏ sᴋɪᴘ!")
+        await message.reply_text("❗ nothing in streaming!")
     else:
-        callsmusic.queues.task_done(chat_id)
+        queues.task_done(chat_id)
 
-        if callsmusic.queues.is_empty(chat_id):
+        if queues.is_empty(chat_id):
             callsmusic.pytgcalls.leave_group_call(chat_id)
         else:
             callsmusic.pytgcalls.change_stream(
-                chat_id, callsmusic.queues.get(chat_id)["file"]
+                chat_id, queues.get(chat_id)["file"]
             )
-            
+
     qeue = que.get(chat_id)
     if qeue:
         skip = qeue.pop(0)
     if not qeue:
         return
-    await message.reply_text(f"➤ sᴋɪᴘᴘᴇᴅ : **{skip[0]}**\n➤ ɴᴏᴡ ᴘʟᴀʏɪɴɢ : **{qeue[0][0]}**")
+    await message.reply_text(f"⫸ skipped : **{skip[0]}**\n⫸ now playing : **{qeue[0][0]}**")
 
 
-@Client.on_message(filters.command("cache"))
-@errors
-async def admincache(client, message: Message):
-    set(
-        message.chat.id,
-        [
-            member.user
-            for member in await message.chat.get_members(filter="administrators")
-        ],
-    )
-    await message.reply_text("✔ **ᴀᴅᴍɪɴ ᴄᴀᴄʜᴇ** ɪs **ʀᴇғʀᴇsʜᴇᴅ**")
+@Client.on_message(filters.command("auth"))
+@authorized_users_only
+async def authenticate(client, message):
+    global admins
+    if not message.reply_to_message:
+        await message.reply("❗ reply to message to authorize user!")
+        return
+    if message.reply_to_message.from_user.id not in admins[message.chat.id]:
+        new_admins = admins[message.chat.id]
+        new_admins.append(message.reply_to_message.from_user.id)
+        admins[message.chat.id] = new_admins
+        await message.reply("user authorized.")
+    else:
+        await message.reply("✅ user already authorized!")
+
+
+@Client.on_message(filters.command("deauth"))
+@authorized_users_only
+async def deautenticate(client, message):
+    global admins
+    if not message.reply_to_message:
+        await message.reply("❗ reply to message to deauthorize user!")
+        return
+    if message.reply_to_message.from_user.id in admins[message.chat.id]:
+        new_admins = admins[message.chat.id]
+        new_admins.remove(message.reply_to_message.from_user.id)
+        admins[message.chat.id] = new_admins
+        await message.reply("user deauthorized")
+    else:
+        await message.reply("✅ user already deauthorized!")

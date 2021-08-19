42d898d74f0d702ab8b11c71d6ff6ed33267d10b
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-19 16:33:03 +0700

    sip

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -1,19 +1,13 @@
 import logging
 from pyrogram.types import Message
-from search_engine_parser import GoogleSearch
 from youtube_search import YoutubeSearch
-from pyrogram import Client as app, filters
+from pyrogram import Client as app
 from helpers.filters import command
 from config import BOT_USERNAME
 
 
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
-
-import pyrogram
-
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
@@ -21,20 +15,20 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("**/search give video name!**")
+            await message.reply_text("/search needs an argument!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("🔎 **sᴇᴀʀᴄʜɪɴɢ ᴠɪᴅᴇᴏ**")
-        results = YoutubeSearch(query, max_results=4).to_dict()
+        m = await message.reply_text("🔎 **Searching**")
+        results = YoutubeSearch(query, max_results=5).to_dict()
         i = 0
         text = ""
-        while i < 4:
-            text += f"**Title:** `{results[i]['title']}`\n"
-            text += f"**Duration:** {results[i]['duration']}\n"
-            text += f"**Viewers:** {results[i]['views']}\n"
+        while i < 5:
+            text += f"**Judul:** `{results[i]['title']}`\n"
+            text += f"**Durasi:** {results[i]['duration']}\n"
+            text += f"**Views:** {results[i]['views']}\n"
             text += f"**Channel:** {results[i]['channel']}\n"
             text += f"https://www.youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
-        await message.reply_text(str(e))
+        await m.edit(str(e))

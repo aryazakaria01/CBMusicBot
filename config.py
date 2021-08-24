e53efe9b709b6847d91a4c94674ba3f2c163168c
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-24 07:32:45 +0700

    clear

--- config.py
+++ config.py
@@ -25,6 +25,9 @@ OWNER_NAME = getenv("OWNER_NAME", "Badboyanim") # isi dengan username kamu tanpa
 DEV_NAME = getenv("DEV_NAME", "Badboyanim")
 PMPERMIT = getenv("PMPERMIT", None)
 
+DATABASE_URL = os.environ.get("DATABASE_URL") # fill with your mongodb url
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL")) # make a private channel and get the id
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False)) # just fill with True or False (optional)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "250"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())

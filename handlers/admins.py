86da723877aaf43704f85f37627942cebc10462d
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-25 13:16:45 +0700

    Version 6,5

--- handlers/admins.py
+++ handlers/admins.py
@@ -1,17 +1,37 @@
 # Copyright (C) 2021 CyberMusicProject
 
-
+import traceback
+import asyncio
 from asyncio import QueueEmpty
 from config import que
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, Chat, CallbackQuery
 
 from cache.admins import admins
 from helpers.channelmusic import get_chat_id
 from helpers.decorators import authorized_users_only, errors
 from helpers.filters import command, other_filters
 from callsmusic import callsmusic
+from callsmusic import groupcall
 from callsmusic.queues import queues
+from config import LOG_CHANNEL, OWNER_ID, BOT_USERNAME
+from helpers.database import db, dcmdb, Database
+from helpers.dbtools import handle_user_status, delcmd_is_on, delcmd_on, delcmd_off
+
+
+@Client.on_message()
+async def _(bot: Client, cmd: Message):
+    await handle_user_status(bot, cmd)
+
+# Back Button
+BACK_BUTTON = InlineKeyboardMarkup([[InlineKeyboardButton("🏡 BACK", callback_data="cbback")]])
+
+@Client.on_message(filters.text & ~filters.private)
+async def delcmd(_, message: Message):
+    if await delcmd_is_on(message.chat.id) and message.text.startswith("/") or message.text.startswith("!"):
+        await message.delete()
+    await message.continue_propagation()
 
 
 @Client.on_message(filters.command("reload"))
@@ -23,6 +43,49 @@ async def update_admin(client, message):
         new_admins.append(u.user.id)
     admins[message.chat.id] = new_admins
     await message.reply_text("✅ Bot **reloaded correctly !**\n✅ **Admin list** has been **updated !**")
+
+
+# Control Menu Of Player
+@Client.on_message(command(["control", f"control@{BOT_USERNAME}", "p"]))
+@errors
+@authorized_users_only
+async def controlset(_, message: Message):
+    await message.reply_text(
+        "**💡 Music player control menu opened !**",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "⏸ Pause music", callback_data="cbpause"
+                    ),
+                    InlineKeyboardButton(
+                        "▶️ Resume music", callback_data="cbresume"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "⏩ Skip music", callback_data="cbskip"
+                    ),
+                    InlineKeyboardButton(
+                        "⏹ End music", callback_data="cbend"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🔇 Mute player", callback_data="cbmute"
+                    ),
+                    InlineKeyboardButton(
+                        "🔊 Unmute player", callback_data="cbunmute"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🗑 Del cmd", callback_data="cbdelcmds"
+                    )
+                ]
+            ]
+        )
+    )
 
 
 @Client.on_message(command("pause") & other_filters)
@@ -126,3 +189,129 @@ async def deautenticate(client, message):
         await message.reply("🔴 User deauthorized.\n\nfrom now that's user can't use the admin commands.")
     else:
         await message.reply("✅ User already deauthorized!")
+
+
+# this is a anti cmd feature
+@Client.on_message(filters.command(["delcmd", f"delcmd@{BOT_USERNAME}"]) & ~filters.private)
+@authorized_users_only
+async def delcmdc(_, message: Message):
+    if len(message.command) != 2:
+        await message.reply_text("Read the /help message to know how to use this command")
+        return
+    status = message.text.split(None, 1)[1].strip()
+    status = status.lower()
+    chat_id = message.chat.id
+    if status == "on":
+        if await delcmd_is_on(message.chat.id):
+            await message.reply_text("✅ Already activated")
+            return
+        else:
+            await delcmd_on(chat_id)
+            await message.reply_text(
+                "🟢 Activated successfully"
+            )
+    elif status == "off":
+        await delcmd_off(chat_id)
+        await message.reply_text("🔴 Disabled successfully")
+    else:
+        await message.reply_text(
+            "Read the /help message to know how to use this command"
+        )
+
+
+@Client.on_message(command(["silent", f"silent@{BOT_USERNAME}", "m"]))
+@errors
+@authorized_users_only
+async def silent(_, message: Message):
+    result = groupcall.mute(message.chat.id)
+
+    if result == 0:
+        await message.reply_text("🔇 Assistant muted")
+    elif result == 1:
+        await message.reply_text("✅ Assistant already muted")
+    elif result == 2:
+        await message.reply_text("❗️ Not connected to voice chat")
+
+
+@Client.on_message(command(["unsilent", f"unsilent@{BOT_USERNAME}", "um"]))
+@errors
+@authorized_users_only
+async def unsilent(_, message: Message):
+    result = groupcall.unmute(message.chat.id)
+
+    if result == 0:
+        await message.reply_text("🔊 Assistant unmuted")
+    elif result == 1:
+        await message.reply_text("✅ Assistant already unmuted")
+    elif result == 2:
+        await message.reply_text("❗️ Not connected to voice chat")
+
+
+# music player callbacks (control by buttons feature)
+
+@Client.on_callback_query(filters.regex("cbpause"))
+async def cbpause(_, query: CallbackQuery):
+    if groupcall.pause(query.message.chat.id):
+        await query.edit_message_text("⏸ Music paused", reply_markup=BACK_BUTTON)
+    else:
+        await query.edit_message_text("❗️ Nothing is playing", reply_markup=BACK_BUTTON)
+
+@Client.on_callback_query(filters.regex("cbresume"))
+async def cbresume(_, query: CallbackQuery):
+    if groupcall.resume(query.message.chat.id):
+        await query.edit_message_text("▶ Music resumed", reply_markup=BACK_BUTTON)
+    else:
+        await query.edit_message_text("❗️ Nothing is paused", reply_markup=BACK_BUTTON)
+
+@Client.on_callback_query(filters.regex("cbend"))
+async def cbend(_, query: CallbackQuery):
+    if query.message.chat.id not in groupcall.active_chats:
+        await query.edit_message_text("❗️ Nothing is playing", reply_markup=BACK_BUTTON)
+    else:
+        try:
+            queues.clear(query.message.chat.id)
+        except QueueEmpty:
+            pass
+
+        await groupcall.stop(query.message.chat.id)
+        await query.edit_message_text("✅ Cleared the queue and left the voice chat!", reply_markup=BACK_BUTTON)
+
+@Client.on_callback_query(filters.regex("cbskip"))
+async def cbskip(_, query: CallbackQuery):
+     if query.message.chat.id not in groupcall.active_chats:
+        await query.edit_message_text("❗️ Nothing is playing", reply_markup=BACK_BUTTON)
+     else:
+        queues.task_done(query.message.chat.id)
+        
+        if queues.is_empty(query.message.chat.id):
+            await groupcall.stop(query.message.chat.id)
+        else:
+            await groupcall.set_stream(
+                query.message.chat.id, queues.get(query.message.chat.id)["file"]
+            )
+
+        await query.edit_message_text("⏭ Skipped to the next music", reply_markup=BACK_BUTTON)
+
+@Client.on_callback_query(filters.regex("cbmute"))
+async def cbmute(_, query: CallbackQuery):
+    result = groupcall.mute(query.message.chat.id)
+
+    if result == 0:
+        await query.edit_message_text("🔇 Assistant muted", reply_markup=BACK_BUTTON)
+    elif result == 1:
+        await query.edit_message_text("✅ Assistant already muted", reply_markup=BACK_BUTTON)
+    elif result == 2:
+        await query.edit_message_text("❗️ Not connected to voice chat", reply_markup=BACK_BUTTON)
+
+@Client.on_callback_query(filters.regex("cbunmute"))
+async def cbunmute(_, query: CallbackQuery):
+    result = groupcall.unmute(query.message.chat.id)
+
+    if result == 0:
+        await query.edit_message_text("🔊 Assistant unmuted", reply_markup=BACK_BUTTON)
+    elif result == 1:
+        await query.edit_message_text("✅ Assistant already unmuted", reply_markup=BACK_BUTTON)
+    elif result == 2:
+        await query.edit_message_text("❗️ Not connected to voice chat", reply_markup=BACK_BUTTON)
+
+# (C) supun-maduraga for his project on call-music-plus 

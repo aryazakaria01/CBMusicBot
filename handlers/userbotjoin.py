77bc7c3cd2facee867c8d44b680e5589ac153d48
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-26 12:40:21 +0700

    Update userbotjoin.py

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -72,4 +72,46 @@ async def bye(client, message):
             await asyncio.sleep(0.7)
         await client.send_message(message.chat.id, f"Left {left} chats. Failed {failed} chats.")
 
-# Idon'tknowwhatisthis
+@Client.on_message(filters.command(["userbotjoinchannel","ubjoinc"]) & ~filters.private & ~filters.bot)
+@authorized_users_only
+@errors
+async def addcchannel(client, message):
+    try:
+      conchat = await client.get_chat(message.chat.id)
+      conid = conchat.linked_chat.id
+      chid = conid
+    except:
+      await message.reply("Is the chat even linked ?")
+      return    
+    chat_id = chid
+    try:
+        invitelink = await client.export_chat_invite_link(chid)
+    except:
+        await message.reply_text(
+            "<b>Promote me as group admin first !</b>",
+        )
+        return
+
+    try:
+        user = await USER.get_me()
+    except:
+        user.first_name = "helper"
+
+    try:
+        await USER.join_chat(invitelink)
+        await USER.send_message(message.chat.id, "🤖: I joined here as you requested")
+    except UserAlreadyParticipant:
+        await message.reply_text(
+            "<b>Helper already in your channel</b>",
+        )
+        return
+    except Exception as e:
+        print(e)
+        await message.reply_text(
+            f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your channel due to heavy join requests for userbot! Make sure user is not banned in channel."
+            f"\n\nOr manually add @{ASSISTANT_NAME} to your Group and try again</b>",
+        )
+        return
+    await message.reply_text(
+        "<b>Helper userbot joined your channel</b>",
+    )

ac0d419ce5f44eda6caa6dfdbd686157c296f83a
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-26 12:37:42 +0700

    Update entertainment.py

--- handlers/entertainment.py
+++ handlers/entertainment.py
@@ -10,7 +10,7 @@ from helpers.filters import command
 @Client.on_message(command(["asupan", f"asupan@{BOT_USERNAME}"]))
 async def asupan(client, message):
     try:
-        resp = requests.get("https://tede-api.herokuapp.com/api/asupan/ptl").json()
+        resp = requests.get("https://api-tede.herokuapp.com/api/asupan/ptl").json()
         results = f"{resp['url']}"
         return await client.send_video(message.chat.id, video=results)
     except Exception:
@@ -20,7 +20,7 @@ async def asupan(client, message):
 @Client.on_message(command(["wibu", f"wibu@{BOT_USERNAME}"]))
 async def wibu(client, message):
     try:
-        resp = requests.get("https://tede-api.herokuapp.com/api/asupan/wibu").json()
+        resp = requests.get("https://api-tede.herokuapp.com/api/asupan/wibu").json()
         results = f"{resp['url']}"
         return await client.send_video(message.chat.id, video=results)
     except Exception:
@@ -30,7 +30,7 @@ async def wibu(client, message):
 @Client.on_message(command(["chika", f"chika@{BOT_USERNAME}"]))
 async def chika(client, message):
     try:
-        resp = requests.get("https://tede-api.herokuapp.com/api/chika").json()
+        resp = requests.get("https://api-tede.herokuapp.com/api/chika").json()
         results = f"{resp['url']}"
         return await client.send_video(message.chat.id, video=results)
     except Exception:
@@ -40,7 +40,7 @@ async def chika(client, message):
 @Client.on_message(command(["truth", f"truth@{BOT_USERNAME}"]))
 async def truth(client, message):
     try:
-        resp = requests.get("https://tede-api.herokuapp.com/api/truth").json()
+        resp = requests.get("https://api-tede.herokuapp.com/api/truth").json()
         results = f"{resp['message']}"
         return await message.reply_text(results)
     except Exception:
@@ -50,7 +50,7 @@ async def truth(client, message):
 @Client.on_message(command(["dare", f"dare@{BOT_USERNAME}"]))
 async def dare(client, message):
     try:
-        resp = requests.get("https://tede-api.herokuapp.com/api/dare").json()
+        resp = requests.get("https://api-tede.herokuapp.com/api/dare").json()
         results = f"{resp['message']}"
         return await message.reply_text(results)
     except Exception:
@@ -65,7 +65,7 @@ async def lirik(_, message):
             return
         query = message.text.split(None, 1)[1]
         rep = await message.reply_text("✈ **sᴇᴀʀᴄʜɪɴɢ ʟʏʀɪᴄs...**")
-        resp = requests.get(f"https://tede-api.herokuapp.com/api/lirik?l={query}").json()
+        resp = requests.get(f"https://api-tede.herokuapp.com/api/lirik?l={query}").json()
         result = f"{resp['data']}"
         await rep.edit(result)
     except Exception:

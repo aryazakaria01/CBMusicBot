6c9c0e6aebdf8bbe2a223faa6198f31ee41be236
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-26 12:38:29 +0700

    Update callsmusic.py

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -1,10 +1,10 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
-import config
+from config import SESSION_NAME, API_ID, API_HASH
 from . import queues
 
-client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
+client = Client(SESSION_NAME, API_ID, API_HASH)
 pytgcalls = PyTgCalls(client)
 
 

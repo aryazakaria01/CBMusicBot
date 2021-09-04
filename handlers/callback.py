e2706d449b9b65e154ca28944d3be2023a67dcaf
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-09-04 10:59:50 +0700

    nothing

--- handlers/callback.py
+++ handlers/callback.py
@@ -37,7 +37,7 @@ async def cbstart(_, query: CallbackQuery):
                         "📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}")
                 ],[
                     InlineKeyboardButton(
-                        "🌐 Off Topic", url="https://t.me/animeeindonesia")
+                        "🌐 Github Page", url="https://github.com/aryazakaria01")
                 ],[
                     InlineKeyboardButton(
                         "🧪 Source Code 🧪", url="https://github.com/aryazakaria01/CBMusicBot"
@@ -98,24 +98,24 @@ async def cbbasic(_, query: CallbackQuery):
     await query.edit_message_text(
         f"""<b>🏮 here is the basic commands</b>
 🎧 [ GROUP VC CMD ]
-/play (song name) - play song from youtube
-/ytp (song name) - play song directly from youtube 
-/stream (reply to audio) - play song using audio file
-/playlist - show the list song in queue
-/song (song name) - download song from youtube
-/search (video name) - search video from youtube detailed
-/vsong (video name) - download video from youtube detailed
-/lyric - (song name) lyrics scrapper
-/vk (song name) - download song from inline mode
+/play (Song name) - Play song from youtube
+/ytp (Song name) - Play song directly from youtube 
+/stream (Reply to audio) - Play song using audio file
+/playlist - Show the list song in queue
+/song (Song name) - Download song from youtube
+/search (Video name) - Search video from youtube detailed
+/vsong (Video name) - Download video from youtube detailed
+/lyric - (Song name) Lyrics scrapper
+/vk (Song name) - Download song from inline mode
 🎧 [ CHANNEL VC CMD ]
-/cplay - stream music on channel voice chat
-/cplayer - show the song in streaming
-/cpause - pause the streaming music
-/cresume - resume the streaming was paused
-/cskip - skip streaming to the next song
-/cend - end the streaming music
-/admincache - refresh the admin cache
-/ubjoinc - invite the assistant for join to your channel
+/cplay - Stream music on channel voice chat
+/cplayer - Show the song in streaming
+/cpause - Pause the streaming music
+/cresume - Resume the streaming was paused
+/cskip - Skip streaming to the next song
+/cend - End the streaming music
+/admincache - Refresh the admin cache
+/ubjoinc - Invite the assistant for join to your channel
 ⚡ __Powered by {BOT_NAME} A.I__""",
         reply_markup=InlineKeyboardMarkup(
             [
@@ -133,11 +133,11 @@ async def cbbasic(_, query: CallbackQuery):
 async def cbadvanced(_, query: CallbackQuery):
     await query.edit_message_text(
         f"""<b>🏮 Here is the advanced commands</b>
-/start (in group) - see the bot alive status
-/reload - reload bot and refresh the admin list
-/cache - refresh the admin cache
-/ping - check the bot ping status
-/uptime - check the bot uptime status
+/start (In group) - See the bot alive status
+/reload - Reload bot and refresh the admin list
+/cache - Refresh the admin cache
+/ping - Check the bot ping status
+/uptime - Check the bot uptime status
 ⚡ __Powered by {BOT_NAME} A.I__""",
         reply_markup=InlineKeyboardMarkup(
             [
@@ -155,21 +155,21 @@ async def cbadvanced(_, query: CallbackQuery):
 async def cbadmin(_, query: CallbackQuery):
     await query.edit_message_text(
         f"""<b>🏮 Here is the admin commands</b>
-/player - show the music playing status
-/pause - pause the music streaming
-/resume - resume the music was paused
-/skip - skip to the next song
-/end - stop music streaming
-/userbotjoin - invite assistant join to your group
-/auth - authorized user for using music bot
-/deauth - unauthorized for using music bot
-/control - open the player settings panel
-/delcmd (on | off) - enable / disable del cmd feature
-/musicplayer (on / off) - disable / enable music player in your group
-/b and /tb (ban / temporary ban) - banned permanently or temporarily banned user in group
-/ub - to unbanned user you're banned from group
-/m and /tm (mute / temporary mute) - mute permanently or temporarily muted user in group
-/um - to unmute user you're muted in group
+/player - Show the music playing status
+/pause - Pause the music streaming
+/resume - Resume the music was paused
+/skip - Skip to the next song
+/end - Stop music streaming
+/userbotjoin - Invite assistant join to your group
+/auth - Authorized user for using music bot
+/deauth - Unauthorized for using music bot
+/control - Open the player settings panel
+/delcmd (on | off) - Enable / disable del cmd feature
+/musicplayer (on / off) - Disable / enable music player in your group
+/b and /tb (ban / temporary ban) - Banned permanently or temporarily banned user in group
+/ub - To unbanned user you're banned from group
+/m and /tm (mute / temporary mute) - Mute permanently or temporarily muted user in group
+/um - To unmute user you're muted in group
 ⚡ __Powered by {BOT_NAME} A.I__""",
         reply_markup=InlineKeyboardMarkup(
             [
@@ -187,9 +187,11 @@ async def cbadmin(_, query: CallbackQuery):
 async def cbsudo(_, query: CallbackQuery):
     await query.edit_message_text(
         f"""<b>🏮 Here is the sudo commands</b>
-/userbotleaveall - order the assistant to leave from all group
-/gcast - send a broadcast message trought the assistant
-/stats - show the bot statistic
+/userbotleaveall - Order the assistant to leave from all group
+/gcast - Send a broadcast message trought the assistant
+/stats - Show the bot statistic
+/rmd - Remove all downloaded files
+/clean - Remove all raw files
 ⚡ __Powered by {BOT_NAME} A.I__""",
         reply_markup=InlineKeyboardMarkup(
             [
@@ -207,12 +209,12 @@ async def cbsudo(_, query: CallbackQuery):
 async def cbowner(_, query: CallbackQuery):
     await query.edit_message_text(
         f"""<b>🏮 Here is the owner commands</b>
-/stats - show the bot statistic
-/broadcast - send a broadcast message from bot
-/block (user id - duration - reason) - block user for using your bot
-/unblock (user id - reason) - unblock user you blocked for using your bot
-/blocklist - show you the list of user was blocked for using your bot
-📝 note: all commands owned by this bot can be executed by the owner of the bot without any exceptions.
+/stats - Show the bot statistic
+/broadcast - Send a broadcast message from bot
+/block (user id - duration - reason) - Block user for using your bot
+/unblock (user id - reason) - Unblock user you blocked for using your bot
+/blocklist - Show you the list of user was blocked for using your bot
+📝 Note: all commands owned by this bot can be executed by the owner of the bot without any exceptions.
 ⚡ __Powered by {BOT_NAME} A.I__""",
         reply_markup=InlineKeyboardMarkup(
             [
@@ -230,11 +232,11 @@ async def cbowner(_, query: CallbackQuery):
 async def cbfun(_, query: CallbackQuery):
     await query.edit_message_text(
         f"""<b>🏮 Here is the fun commands</b>
-/chika - check it by yourself
-/wibu - check it by yourself
-/asupan - check it by yourself
-/truth - check it by yourself
-/dare - check it by yourself
+/chika - Check it by yourself
+/wibu - Check it by yourself
+/asupan - Check it by yourself
+/truth - Check it by yourself
+/dare - Check it by yourself
 ⚡ __Powered by {BOT_NAME} A.I__""",
         reply_markup=InlineKeyboardMarkup(
             [
@@ -275,7 +277,6 @@ async def cbguide(_, query: CallbackQuery):
 
 
 @Client.on_callback_query(filters.regex("close"))
-@cb_admin_check
 async def close(_, query: CallbackQuery):
     await query.message.delete()
 
@@ -364,10 +365,10 @@ async def cbdelcmds(_, query: CallbackQuery):
 **💡 Feature:** delete every commands sent by users to avoid spam in groups !
 ❔ Usage:**
  1️⃣ To turn on feature:
-     » type `/delcmd on`
+     » Type `/delcmd on`
     
  2️⃣ To turn off feature:
-     » type `/delcmd off`
+     » Type `/delcmd off`
       
 ⚡ __Powered by {BOT_NAME} A.I__""",
         reply_markup=InlineKeyboardMarkup(

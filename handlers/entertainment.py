a390b564778483e55452cc02fc1a1163f132a085
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-09-04 11:06:31 +0700

    fixed

--- handlers/entertainment.py
+++ handlers/entertainment.py
@@ -1,6 +1,6 @@
 # Module by https://github.com/tofikdn
 # Copyright (C) 2021 TdMusic
-# Ported by @aryazakaria01 for CBMusicBot
+# Ported by @aryazakaria01 for CBMusicProject
 
 import requests
 from pyrogram import Client
@@ -54,17 +54,17 @@ async def dare(client, message):
         results = f"{resp['message']}"
         return await message.reply_text(results)
     except Exception:
-        await message.reply_text("something went wrong...")
+        await message.reply_text("Something went wrong...")
 
 
 @Client.on_message(command(["lyric", f"lyric@{BOT_USERNAME}"]))
 async def lirik(_, message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("**give a lyric name too !**")
+            await message.reply_text("**Give a lyric name too !**")
             return
         query = message.text.split(None, 1)[1]
-        rep = await message.reply_text("🔎 **searching lyrics...**")
+        rep = await message.reply_text("🔎 **Searching lyrics...**")
         resp = requests.get(f"https://api-tede.herokuapp.com/api/lirik?l={query}").json()
         result = f"{resp['data']}"
         await rep.edit(result)

9e1b2f0949a683f5957c676ec1a04391e9a9e1cb
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-09-04 11:47:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ if os.path.exists("local.env"):
 
 load_dotenv()
 que = {}
+admins = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME", "ᴄʏʙᴇʀ ᴍᴜsɪᴄ ʙᴏᴛ")
@@ -14,7 +15,6 @@ BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/6790864f5fe27471bdc8d.png
 THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/e9a4d6655e5ddf51f9160.jpg")
 AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/91034f175d41040d45b38.jpg")
 QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/c8a0e9c544c5ea689caf9.jpg")
-admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_USERNAME = getenv("BOT_USERNAME", "CyberMusikBot")
@@ -23,13 +23,11 @@ GROUP_SUPPORT = getenv("GROUP_SUPPORT", "CyberSupportGroup")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "CyberMusicProject")
 OWNER_NAME = getenv("OWNER_NAME", "Badboyanim") # isi dengan username kamu tanpa simbol @
 PMPERMIT = getenv("PMPERMIT", None)
-
 OWNER_ID = int(os.environ.get("OWNER_ID")) # fill with your id as the owner of the bot
 DATABASE_URL = os.environ.get("DATABASE_URL") # fill with your mongodb url
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL")) # make a private channel and get the channel id
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False)) # just fill with True or False (optional)
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "250"))
-
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+LANG = getenv("LANG", "id")
